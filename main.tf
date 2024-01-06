5b9b9df4f18c8c122a08456c0b20110499f664b5
Pim Merks <pimmerks@users.noreply.github.com>
2024-01-06 13:06:20 +0100

    Allow more pods per node

--- main.tf
+++ main.tf
@@ -54,6 +54,18 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
+
+    # Allow more pods per node
+    pre_bootstrap_user_data = <<-EOT
+  #!/bin/bash
+  set -ex
+  cat <<-EOF > /etc/profile.d/bootstrap.sh
+  export USE_MAX_PODS=false
+  export KUBELET_EXTRA_ARGS="--max-pods=110"
+  EOF
+  # Source extra environment variables in bootstrap script
+  sed -i '/^set -o errexit/a\\nsource /etc/profile.d/bootstrap.sh' /etc/eks/bootstrap.sh
+  EOT
   }
 
   eks_managed_node_groups = {
